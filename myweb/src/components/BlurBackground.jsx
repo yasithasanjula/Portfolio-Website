2c72d1c9a308617fe9c1f7cc7090188b2cd3e50c
Yasitha Sanjula Herath <yasithasanjula2018@gmail.com>
2024-11-20 11:49:15 +0530

    1111

--- myweb/src/components/BlurBackground.jsx
+++ myweb/src/components/BlurBackground.jsx
@@ -7,62 +7,57 @@ const circleVariants = {
     y: [0, 50, 0],
     transition: {
       duration: 2,
-      repeat: Infinitiy,
+      repeat: Infinity,
       ease: "easeInOut",
     },
   },
-
   animate2: {
     scale: [1, 1.1, 1],
     x: [0, 120, 0],
     y: [0, -60, 0],
     transition: {
       duration: 2,
-      repeat: Infinitiy,
+      repeat: Infinity,
       ease: "easeInOut",
     },
   },
-
   animate3: {
     scale: [1, 1.3, 1],
     x: [0, 140, 0],
     y: [0, 70, 0],
     transition: {
       duration: 3,
-      repeat: Infinitiy,
+      repeat: Infinity,
       ease: "easeInOut",
     },
   },
-
   animate4: {
     scale: [1, 1.4, 1],
     x: [0, 160, 0],
     y: [0, -80, 0],
     transition: {
       duration: 4,
-      repeat: Infinitiy,
+      repeat: Infinity,
       ease: "easeInOut",
     },
   },
-
   animate5: {
     scale: [1, 1.5, 1],
     x: [0, 180, 0],
     y: [0, 90, 0],
     transition: {
       duration: 3,
-      repeat: Infinitiy,
+      repeat: Infinity,
       ease: "easeInOut",
     },
   },
-
   animate6: {
     scale: [1, 1.6, 1],
     x: [0, 200, 0],
     y: [0, -100, 0],
     transition: {
       duration: 3,
-      repeat: Infinitiy,
+      repeat: Infinity,
       ease: "easeInOut",
     },
   },
@@ -88,6 +83,21 @@ const BlurBackground = () => {
         className="bg-green-500 w-[350px] h-[250px] rounded-full absolute"
         variants={circleVariants}
         animate="animate3"
+      ></motion.div>
+      <motion.div
+        className="bg-purple-500 w-[350px] h-[250px] rounded-full absolute"
+        variants={circleVariants}
+        animate="animate4"
+      ></motion.div>
+      <motion.div
+        className="bg-blue-500 w-[350px] h-[250px] rounded-full absolute"
+        variants={circleVariants}
+        animate="animate5"
+      ></motion.div>
+      <motion.div
+        className="bg-green-500 w-[350px] h-[250px] rounded-full absolute"
+        variants={circleVariants}
+        animate="animate6"
       ></motion.div>
     </div>
   );
