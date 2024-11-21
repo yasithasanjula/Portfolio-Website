214642f1212a6668da0a6ceebc408536036a6943
Yasitha Sanjula Herath <yasithasanjula2018@gmail.com>
2024-11-21 07:17:47 +0530

    111

--- myweb/src/components/Hero.jsx
+++ myweb/src/components/Hero.jsx
@@ -1,4 +1,6 @@
 import { motion, stagger } from "framer-motion";
+import { HERO_CONTENT } from "../constants/index";
+import jason from "../assets/jason-root.webp";
 
 const textVariants = {
   hidden: { opacity: 0, y: 50 },
@@ -19,7 +21,65 @@ const imageVariants = {
 };
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section>
+      <div className="relative z-10 flex flex-col flex-wrap items-center justify-center min-h-screen text-white md:flex-row">
+        <motion.div
+          className="w-full p-8 md:w-1/2"
+          initial="hidden"
+          animate="visible"
+          variants={containerVariants}
+        >
+          <motion.h1
+            className="text-2xl md:text-3xl lg:text-5xl my-14"
+            variants={textVariants}
+          >
+            {HERO_CONTENT.greeting}
+          </motion.h1>
+
+          <motion.p
+            className="mb-4 text-xl md:text-2xl lg:text-4xl"
+            variants={textVariants}
+          >
+            {HERO_CONTENT.introduction}
+          </motion.p>
+
+          <motion.p
+            className="text-xl md:text-2xl lg:text-4xl"
+            variants={textVariants}
+          >
+            {HERO_CONTENT.description}
+          </motion.p>
+
+          <motion.a
+            className="inline-block p-3 mt-8 bg-stone-50 text-stone-900 lg:p-4 rounded-2xl"
+            href={HERO_CONTENT.resumeLink}
+            download
+            rel=" noopener noreferrer"
+            target="_blank"
+            variants={textVariants}
+          >
+            {HERO_CONTENT.resumeLinkText}
+          </motion.a>
+        </motion.div>
+
+        <motion.div
+          className="w-full p-8 md:w-1/2"
+          initial="hidden"
+          animate="visible"
+          variants={imageVariants}
+        >
+          <img
+            src={jason}
+            alt="Jason Root"
+            width={650}
+            height={650}
+            className="rounded-3xl"
+          />
+        </motion.div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
